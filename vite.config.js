487e4435c817d001f55780a6fbbba6410f82bf28
prabavathimagesh <prabavathi3898@gmail.com>
2025-07-17 15:38:39 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: ['framer-motion']
+    }
+  }
+}
